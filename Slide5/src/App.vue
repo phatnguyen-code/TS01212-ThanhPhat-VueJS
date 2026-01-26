fd3b34aa358521a9e109120e0763a5c913960f12
phatnguyen-code <phatntts01212@fpt.edu.vn>
2026-01-26 22:34:50 +0700

    Hoàn thành bài1

---
+++ Slide5/src/App.vue
@@ -0,0 +1,40 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import Bai1 from './components/Bai1.vue';
+import Bai2 from './components/Bai2.vue';
+import Bai3 from './components/Bai3.vue';
+import {ref} from 'vue'
+import CreatePost from './components/CreatePost.vue';
+import PostList from './components/PostList.vue';
+
+const posts = ref([])
+
+function addPost(post) {
+  posts.value.push(post)
+}
+</script>
+
+<template>
+  <div id="app">
+    <h1>Ứng dụng Blog nhỏ với Vue.js</h1>
+    <CreatePost @add-post="addPost" />
+    <PostList :posts="posts" /> 
+  </div>
+  <!--<div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />-->
+</template>
+
+<style>
+#app {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+}
+</style>
