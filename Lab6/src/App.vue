30348c1b07d16ab9059ef1177565aabbc898351a
phatnguyen-code <phatntts01212@fpt.edu.vn>
2026-01-30 21:39:30 +0700

    Hoàn thành bài4

--- Lab6/src/App.vue
+++ Lab6/src/App.vue
@@ -3,13 +3,11 @@ import HelloWorld from './components/HelloWorld.vue'
 import Bai1 from './components/Bai1.vue';
 import Bai2 from './components/Bai2.vue';
 import Bai3 from './components/Bai3.vue';
+import Bai4 from './components/Bai4.vue';
 </script>
 
 <template>
-  <Bai3/>
-  <div class="container py-5">
-    <h1 class="text-center text-uppercase fw-bold mb-5">Thực hành Lab 6 - VueJS</h1>
-  </div>
+  <Bai4/>
 </template>
 
 <style scoped>
